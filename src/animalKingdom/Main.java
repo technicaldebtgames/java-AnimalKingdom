e3cca36399da96975d45559df2820cd01b3a4f98
Dan Brioli <technicaldebtgames@gmail.com>
2020-08-04 18:04:46 -0400

    typed a bunch of animals, realized my tl is a bot

--- src/animalKingdom/Main.java
+++ src/animalKingdom/Main.java
@@ -1,5 +1,8 @@
 package animalKingdom;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main 
 {
 
@@ -8,6 +11,24 @@ public class Main
 
         System.out.println("Animal Kingdom Start -->");
         
+        List<Animal> animalList = new ArrayList<>(
+            List.of(new Mammal("Panda", 1869),
+                    new Mammal("Zebra", 1778),
+                    new Mammal("Koala", 1816),
+                    new Mammal("Sloth", 1804),
+                    new Mammal("Armadillo", 1758),
+                    new Mammal("Raccoon", 1758),
+                    new Mammal("Bigfoot", 2021),
+                    new Bird("Pigeon", 1837),
+                    new Bird("Peacock", 1821),
+                    new Bird("Toucan", 1758),
+                    new Bird("Parrot", 1824),
+                    new Bird("Swan", 1758),
+                    new Fish("Salmon", 1758),
+                    new Fish("Catfish", 1817),
+                    new Fish("Perch", 1758))
+        );
+
         System.out.println("<-- Animal Kingdom End");
 
     }
